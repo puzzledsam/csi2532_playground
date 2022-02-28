2870ea0a76d69bb281d7c1d665a379cc8439c814
Samuel Dion <samuelqwe@icloud.com>
2022-02-28 15:22:59 -0500

    Première migration du DB

---
+++ db/migrations/20220227143500-create-migrations.sql
@@ -0,0 +1,9 @@
+CREATE TABLE schema_migrations ( 
+  migration varchar(255), 
+  migrated_at time, 
+  PRIMARY KEY (migration) 
+); 
+INSERT INTO schema_migrations (migration, migrated_at) VALUES 
+('20220226130000-create-athletes.sql', '2022-02-27 13:30:00'); 
+INSERT INTO schema_migrations (migration, migrated_at) VALUES 
+('20220227143500-create-migrations.sql', '2020-02-27 14:35:00');
---
+++ db/schema.sql
@@ -0,0 +1,16 @@
+CREATE TABLE athletes (
+  id int,
+  identifier varchar(50),
+  created timestamp,
+  modified timestamp,
+  name varchar(50),
+  dob date,
+  identified_gender varchar(6),
+  PRIMARY KEY (id)
+);
+
+CREATE TABLE schema_migrations ( 
+  migration varchar(255), 
+  migrated_at time, 
+  PRIMARY KEY (migration) 
+);
